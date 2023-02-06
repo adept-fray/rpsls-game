6ac36d8ae99cfc6dcea12f72ed4642b2c0499439
adept-fray <siavash.kh41@gmail.com>
2023-02-06 18:02:41 +0000

    Add eventlistener to document contentload & player deck to play one round on click on decks. + Instructions in comment.

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,3 +11,50 @@
 //  check if wins = 3
 //  if yes open new level in session storage & remove disbaled class from next-button
 //  next-button clicked. update difficulty to next level
+
+document.addEventListener('DOMContentLoaded', function contentLoaded(){
+    console.log(difficulty);
+
+    //  add eventlistener to player-deck to start a round
+    let playerDeck = document.getElementsByClassName('deck');
+    for (const hand of playerDeck) {
+        console.log(hand.textContent);
+        hand.addEventListener('click', playRound, false);
+    }
+});
+
+function playRound(ev){
+    ev.preventDefault();
+    let playerHand = this.textContent;
+    let botHand = botPick();
+    console.log('playerHand: ' + playerHand);
+    console.log('botHand: ' + botHand);
+
+    //  check rounds = 0
+    //  round = 0 stop game
+
+    //  check wins = 3
+    //  update sessionStorage.openLevel & remove disbaled class from next-level button
+
+    //  check playerhand & bothand for
+    //  win tie lose
+
+    //  update result accordingly textcontent
+    //  update rounds textcontent
+}
+
+function botPick(random){
+    /*
+    let random = ( Math.floor(Math.random() * 100) + 1 );
+    let difficulty = sessionStorage.difficultyName ? sessionStorage.difficultyName : 'cloud';
+    if(difficulty == 'cloud'){
+        cloudPick(random);
+    } else if(difficulty == 'snowman'){
+        snowmanPick(random);
+    } else if(difficulty == 'dove'){
+        dovePick(random);
+    } else if(difficulty == 'leaf'){
+        leafPick(random);
+    }*/
+    return 'PAPER';
+}
